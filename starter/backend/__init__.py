514e90372bf858a45c1146ea4a2cc0ee475a3260
phucnd12 <ngoduyphuc820@gmail.com>
2024-11-22 17:25:44 +0700

    Update CORS origins

--- starter/backend/__init__.py
+++ starter/backend/__init__.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 from .movies import movies_api
 
 app = Flask(__name__)
-CORS(app, origins=["http://a1db91787e3d54a60b9d795d44e2dffc-1137869864.us-east-1.elb.amazonaws.com", "http://localhost:3000"])
+CORS(app, origins="http://a1db91787e3d54a60b9d795d44e2dffc-1137869864.us-east-1.elb.amazonaws.com")
 app.register_blueprint(movies_api)
 
 # Start app
